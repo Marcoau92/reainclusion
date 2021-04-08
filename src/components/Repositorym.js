cd3493c4ed68acaf0155811bcf84f3dd723f802a
Marco Alvarez <marco.au92@gmail.com>
2021-04-08 03:20:02 -0400

    margenes mobile repository

--- src/components/Repositorym.js
+++ src/components/Repositorym.js
@@ -36,7 +36,7 @@ const useStyles = makeStyles((theme) => ({
                 </Grid>
                 <Wspace/>
                 <Grid md={6} lg={6} justify="flex-start">
-                <p  class="pservices text-justify">
+                <p  class="pservices text-justify" style={{marginLeft: '0.8rem',marginRight: '0.8rem'}}>
                 A continuación podrás encontrar archivos descargables con investigaciones y propuestas para potenciar la inclusión de tus estudiantes. 
 Gran parte de estos documentos corresponden a tesis de pregrado de profesores egresados de la carrera Pedagogía en Comunicación en Lengua Inglesa de la UACh.
 Si quieres colaborar con tu propia investigación o propuesta, no dudes en contactarnos. También puedes hacernos llegar estudios o artículos de otros autores, siempre que sean de uso libre y gratuito.
@@ -46,46 +46,46 @@ Si quieres colaborar con tu propia investigación o propuesta, no dudes en conta
                 <Wspace/>
                 <Grid container style={{ justifyContent: "flex-start"}} md={6} lg={6} >
                     <Router>
-                        <Link to="/repository/Assessing Visually Impaired Students in Inclusive Classrooms (Morales & Soto, 2014).pdf" target="_blank" download>•	 Assessing Visually Impaired Students in Inclusive Classrooms (Morales & Soto, 2014)</Link>
+                        <Link to="/repository/Assessing Visually Impaired Students in Inclusive Classrooms (Morales & Soto, 2014).pdf" target="_blank" download style={{marginLeft: '0.8rem',marginRight: '0.8rem'}}>•	 Assessing Visually Impaired Students in Inclusive Classrooms (Morales & Soto, 2014)</Link>
                     </Router>
                     <Router>
-                        <Link to="/repository/Break dancers in the EFL classroom...(Salinas, 2014).pdf" target="_blank" download>•	 Break dancers in the EFL classroom...(Salinas, 2014)</Link>
+                        <Link to="/repository/Break dancers in the EFL classroom...(Salinas, 2014).pdf" target="_blank" download style={{marginLeft: '0.8rem',marginRight: '0.8rem'}}>•	 Break dancers in the EFL classroom...(Salinas, 2014)</Link>
                     </Router>
                     <Router>
-                        <Link to="/repository/Developing SENAME children's socio-Affective and Communicative Skills (Igor & Pérez, 2020).pdf" target="_blank" download>•	 Developing SENAME children's socio-Affective and Communicative Skills (Igor & Pérez, 2020)</Link>
+                        <Link to="/repository/Developing SENAME children's socio-Affective and Communicative Skills (Igor & Pérez, 2020).pdf" target="_blank" download style={{marginLeft: '0.8rem',marginRight: '0.8rem'}}>•	 Developing SENAME children's socio-Affective and Communicative Skills (Igor & Pérez, 2020)</Link>
                     </Router>
                     <Router>
-                        <Link to="/repository/Drama Techniques as an effective way to teach English to visually impaired students (Castillo & Siebert, 2013).pdf" target="_blank" download>•	 Drama Techniques as an effective way to teach English to visually impaired students (Castillo & Siebert, 2013)</Link>
+                        <Link to="/repository/Drama Techniques as an effective way to teach English to visually impaired students (Castillo & Siebert, 2013).pdf" target="_blank" download style={{marginLeft: '0.8rem',marginRight: '0.8rem'}}>•	 Drama Techniques as an effective way to teach English to visually impaired students (Castillo & Siebert, 2013)</Link>
                     </Router>
                     <Router>
-                        <Link to="/repository/Including children with Asperger Syndrome in an English class...(Navarro, 2011).pdf" target="_blank" download>•	 Including children with Asperger Syndrome in an English class...(Navarro, 2011)</Link>
+                        <Link to="/repository/Including children with Asperger Syndrome in an English class...(Navarro, 2011).pdf" target="_blank" download style={{marginLeft: '0.8rem',marginRight: '0.8rem'}}>•	 Including children with Asperger Syndrome in an English class...(Navarro, 2011)</Link>
                     </Router>
                     <Router>
-                        <Link to="/repository/Introducing EFL through NFE in social risk context (Berkhoff & Gahona, 2014).pdf" target="_blank" download>•	 Introducing EFL through NFE in social risk context (Berkhoff & Gahona, 2014)</Link>
+                        <Link to="/repository/Introducing EFL through NFE in social risk context (Berkhoff & Gahona, 2014).pdf" target="_blank" download style={{marginLeft: '0.8rem',marginRight: '0.8rem'}}>•	 Introducing EFL through NFE in social risk context (Berkhoff & Gahona, 2014)</Link>
                     </Router>
                     <Router>
-                        <Link to="/repository/Principle-based language activities to promote a peaceful coexistence...(Arias & Rivera, 2020).pdf" target="_blank" download>•	 Principle-based language activities to promote a peaceful coexistence...(Arias & Rivera, 2020)</Link>
+                        <Link to="/repository/Principle-based language activities to promote a peaceful coexistence...(Arias & Rivera, 2020).pdf" target="_blank" download style={{marginLeft: '0.8rem',marginRight: '0.8rem'}}>•	 Principle-based language activities to promote a peaceful coexistence...(Arias & Rivera, 2020)</Link>
                     </Router>
                     <Router>
-                        <Link to="/repository/Safe and Inclusive Schools-Teacher's strategies to handle peer-to-peer violence (Soto, 2016).pdf" target="_blank" download>•	 Safe and Inclusive Schools-Teacher's strategies to handle peer-to-peer violence (Soto, 2016)</Link>
+                        <Link to="/repository/Safe and Inclusive Schools-Teacher's strategies to handle peer-to-peer violence (Soto, 2016).pdf" target="_blank" download style={{marginLeft: '0.8rem',marginRight: '0.8rem'}}>•	 Safe and Inclusive Schools-Teacher's strategies to handle peer-to-peer violence (Soto, 2016)</Link>
                     </Router>
                     <Router>
-                        <Link to="/repository/Social risk students-Creating a favourable classroom environment through drama (Acosta & Cuitiño, 2013).pdf" target="_blank" download>•	 Social risk students-Creating a favourable classroom environment through drama (Acosta & Cuitiño, 2013)</Link>
+                        <Link to="/repository/Social risk students-Creating a favourable classroom environment through drama (Acosta & Cuitiño, 2013).pdf" target="_blank" download style={{marginLeft: '0.8rem',marginRight: '0.8rem'}}>•	 Social risk students-Creating a favourable classroom environment through drama (Acosta & Cuitiño, 2013)</Link>
                     </Router>
                     <Router>
-                        <Link to="/repository/Teachers' motivation in the EFL classroom through the implementation of non-traditional methodologies (Luengo & Martínez, 2011).pdf" target="_blank" download>•	 Teachers' motivation in the EFL classroom through the implementation of non-traditional methodologies (Luengo & Martínez, 2011)</Link>
+                        <Link to="/repository/Teachers' motivation in the EFL classroom through the implementation of non-traditional methodologies (Luengo & Martínez, 2011).pdf" target="_blank" download style={{marginLeft: '0.8rem',marginRight: '0.8rem'}}>•	 Teachers' motivation in the EFL classroom through the implementation of non-traditional methodologies (Luengo & Martínez, 2011)</Link>
                     </Router>
                     <Router>
-                        <Link to="/repository/Teaching English in the hospital classroom (Moena & Turra, 2013) .pdf" target="_blank" download>•	 Teaching English in the hospital classroom (Moena & Turra, 2013) </Link>
+                        <Link to="/repository/Teaching English in the hospital classroom (Moena & Turra, 2013) .pdf" target="_blank" download style={{marginLeft: '0.8rem',marginRight: '0.8rem'}}>•	 Teaching English in the hospital classroom (Moena & Turra, 2013) </Link>
                     </Router>
                     <Router>
-                        <Link to="/repository/Teaching strategies to support students with ASD (Castro & Ojeda, 2021).pdf" target="_blank" download>•	 Teaching strategies to support students with ASD (Castro & Ojeda, 2021)</Link>
+                        <Link to="/repository/Teaching strategies to support students with ASD (Castro & Ojeda, 2021).pdf" target="_blank" download style={{marginLeft: '0.8rem',marginRight: '0.8rem'}}>•	 Teaching strategies to support students with ASD (Castro & Ojeda, 2021)</Link>
                     </Router>
                     <Router>
-                        <Link to="/repository/Theatre of the oppressed in the EFL Classroom...(Bilbao & Burgos, 2013).pdf" target="_blank" download>•	 Theatre of the oppressed in the EFL Classroom...(Bilbao & Burgos, 2013)</Link>
+                        <Link to="/repository/Theatre of the oppressed in the EFL Classroom...(Bilbao & Burgos, 2013).pdf" target="_blank" download style={{marginLeft: '0.8rem',marginRight: '0.8rem'}}>•	 Theatre of the oppressed in the EFL Classroom...(Bilbao & Burgos, 2013)</Link>
                     </Router>
                     <Router>
-                        <Link to="/repository/Visually impaired children-An approach to the English learning process ... (Barrientos & Morales, 2011).pdf" target="_blank" download>•	 Visually impaired children-An approach to the English learning process ... (Barrientos & Morales, 2011)</Link>
+                        <Link to="/repository/Visually impaired children-An approach to the English learning process ... (Barrientos & Morales, 2011).pdf" target="_blank" download style={{marginLeft: '0.8rem',marginRight: '0.8rem'}}>•	 Visually impaired children-An approach to the English learning process ... (Barrientos & Morales, 2011)</Link>
                     </Router>
                 </Grid>
 
