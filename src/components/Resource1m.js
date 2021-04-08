ea4348b4d259a655ea1dece19327683fdc0c7b31
Marco Alvarez <marco.au92@gmail.com>
2021-04-08 03:45:37 -0400

    margenes mobile recurso1

--- src/components/Resource1m.js
+++ src/components/Resource1m.js
@@ -41,7 +41,7 @@ const Resources1 = (props) => {
                     <Typography className={classes.subtitle} align='center' variant='h1'>Clases de inglés para estudiantes Sordos</Typography>
                     <Wspace2/>
                     <Grid >
-                    <p  class="pservices text-justify">
+                    <p  class="pservices text-justify" style={{marginLeft: '0.8rem',marginRight: '0.8rem'}}>
                     En el siguiente video el profesor Felipe Martínez te cuenta sobre sus estrategias para hacer clases de inglés con estudiantes Sordos en un aula regular. 
 Este es un extracto de la entrevista. Puedes descargar
 <Router>
@@ -66,12 +66,12 @@ Este es un extracto de la entrevista. Puedes descargar
                     <Typography className={classes.subtitle} align='center' variant='h1'>Profesor Felipe Martínez Corona</Typography>
                     <Grid >
                     <Wspace2/>
-                    <p  class="pservices text-justify">
+                    <p  class="pservices text-justify" style={{marginLeft: '0.8rem',marginRight: '0.8rem'}}>
                         Felipe Martínez Corona es licenciado en Educación y profesor de Comunicación en Lengua Ingles de la Universidad Austral de Chile (2011), tiene un Magister en Comunicación de la Universidad Austral de Chile (2019). Desde el año 2011 se desempeña como profesor adjunto en la Facultad de Filosofía y Humanidades de Universidad Austral de Chile, donde ha realizado clases para diversos cursos de pre y postgrado de la Universidad (Centro de Idiomas).
                     </p>
                     </Grid>
                     
-                    <p md={4} lg={4} class="pservices text-justify">
+                    <p md={4} lg={4} class="pservices text-justify" style={{marginLeft: '0.8rem',marginRight: '0.8rem'}}>
                         Ha colaborado en proyectos de investigación internos de la Universidad Austral como “Inglés, actuando el lenguaje” (2009) e “Inglés, atuando el lenguaje II” (2011) y Proyectos de Extensión Internos (UACh) como Spanglish theatre en tu colegio (2012), también dirigió la iniciativa English Conversation Club del Centro de Idiomas de la Universidad Austral de Chile (2015-2018).
                     </p>
                     <Grid>
@@ -99,42 +99,42 @@ Este es un extracto de la entrevista. Puedes descargar
                     <Wspace/>
                     <Typography className={classes.subtitle} align='center' variant='h1'>Recursos Sugeridos</Typography>
                     <Wspace2/>
-                    <Grid container class="pservices text-justify" >
+                    <Grid container class="pservices text-justify" style={{marginLeft: '0.8rem',marginRight: '0.8rem'}}>
                     El profesor Felipe Martínez recomienda los siguientes recursos para apoyar el proceso de enseñanza-aprendizaje de estudiantes Sordos en el aula de inglés.
                     </Grid>
-                    <Grid container >
+                    <Grid container style={{marginLeft: '0.8rem',marginRight: '0.8rem'}}>
                     •	American Sign Language (ASL): 
                     <a rel="pagina principal" href="https://www.lifeprint.com/asl101/lessons/lesson01.htm " md={6} xs={6} >https://www.lifeprint.com/asl101/lessons/lesson01.htm </a>
                     </Grid>
-                    <Grid container >
+                    <Grid container style={{marginLeft: '0.8rem',marginRight: '0.8rem'}}>
                     •	Diccionario: 
                     <a rel="pagina principal" href="http://asluniversity.com/dictionary.htm " md={6} xs={6} >http://asluniversity.com/dictionary.htm </a> 
                     </Grid>
-                    <Grid container >
+                    <Grid container style={{marginLeft: '0.8rem',marginRight: '0.8rem'}}>
                     •	Videos: 
                     <a rel="pagina principal" href="https://www.youtube.com/watch?v=DaMjr4AfYA0&t=1s" md={6} xs={6} >https://www.youtube.com/watch?v=DaMjr4AfYA0&t=1s</a> 
                     </Grid>
-                    <Grid container >
+                    <Grid container style={{marginLeft: '0.8rem',marginRight: '0.8rem'}} >
                     •	Lectura para Sordos (en español
                     <a rel="pagina principal" href="https://cultura-sorda.org/lectura-sencilla/" md={6} xs={6} >https://cultura-sorda.org/lectura-sencilla/</a> 
                     </Grid>
-                    <Grid container >
+                    <Grid container style={{marginLeft: '0.8rem',marginRight: '0.8rem'}}>
                     •	Verbos in ASL: 
                     <a rel="pagina principal" href="https://www.youtube.com/watch?v=thdb-I-H9kE&t=30s" md={6} xs={6} >https://www.youtube.com/watch?v=thdb-I-H9kE&t=30s</a> 
                     </Grid>
-                    <Grid container >
+                    <Grid container style={{marginLeft: '0.8rem',marginRight: '0.8rem'}}>
                     •	Imágenes de Google: 
                     <a rel="pagina principal" href="https://www.google.com/imghp?hl=en" md={6} xs={6} >https://www.google.com/imghp?hl=en</a> 
                     </Grid>
-                    <Grid container >
+                    <Grid container style={{marginLeft: '0.8rem',marginRight: '0.8rem'}}>
                     •	Recursos visuals gratuitos (puzzles, crossword, etc): 
                     <a rel="pagina principal" href="https://busyteacher.org/" md={6} xs={6} >https://busyteacher.org/</a> 
                     </Grid>
-                    <Grid container >
+                    <Grid container style={{marginLeft: '0.8rem',marginRight: '0.8rem'}} >
                     •	Pictoramas: 
                     <a rel="pagina principal" href="http://aulaabierta.arasaac.org/ " md={6} xs={6} >http://aulaabierta.arasaac.org/ </a> 
                     </Grid>
-                    <Grid container >
+                    <Grid container style={{marginLeft: '0.8rem',marginRight: '0.8rem'}}>
                     •	Imágenes y evaluaciones interactivas: 
                     <a rel="pagina principal" href="https://quizlet.com/" md={6} xs={6} >https://quizlet.com/</a> 
                     </Grid>
