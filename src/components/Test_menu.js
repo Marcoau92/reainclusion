42797c1ecdabeda1fc94a46ce58e6e84bf8a5a0a
Marco Alvarez <marco.au92@gmail.com>
2022-05-11 00:55:14 -0400

    recursos nuevo orden13

--- src/components/Test_menu.js
+++ src/components/Test_menu.js
@@ -39,7 +39,9 @@ const Resources = (props) => {
         return(
             <div >
                 <br></br>
-                 
+                <Grid container item md={4} lg={4}>
+
+</Grid>
                 <Grid md={12} lg={12}>
                 <Typography className={classes.title} align='center' variant='h3'>Recursos</Typography>
 
